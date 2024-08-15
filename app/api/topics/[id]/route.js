6fa6dbb33b1663b858a7cabcea95704dc4338415
Rifat Sarker <rifatswd@gmail.com>
2024-08-15 16:44:25 +0600

    delete,put

--- app/api/topics/[id]/route.js
+++ app/api/topics/[id]/route.js
@@ -1,8 +1,19 @@
+import connectMongoDB from "@/libs/mongodb";
 import Topic from "@/models/topic";
+import { NextResponse } from "next/server";
 
 export async function PUT(req, {params}){
     const {id} = params;
-    const updatedTopic = await Topic.findByIdAndUpdate(id, req.body, {new: true});
     await connectMongoDB();
+    const {title, description} = await req.json();
+    const updatedTopic = await Topic.findByIdAndUpdate(id, {title, description}, {new: true});
     return NextResponse.json({updatedTopic}, {status: 200});
+}
+
+
+export async function GET(req, {params}){
+    const {id} = params;
+    await connectMongoDB();
+    const topic = await Topic.findOne({_id: id});
+    return NextResponse.json({topic}, {status: 200})
 }
